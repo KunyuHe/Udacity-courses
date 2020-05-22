29f5d3d59a4ac83a440b370a823ee3c1a2ffed7c
Kunyu He <kunyuhe@uchicago.edu>
2020-05-22 13:36:50 -0500

    Wrapped up project 2

--- Data Structure and Algorithms Nanodegree/Projects/Data Structures/Problem 1 - LRU Cache/test.py
+++ Data Structure and Algorithms Nanodegree/Projects/Data Structures/Problem 1 - LRU Cache/test.py
@@ -2,52 +2,70 @@ import pytest
 
 from LRU_Cache import LRU_Cache
 
+TEST_NORMAL = [
+    ((5, [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6)], 1),
+     -1),
 
-def create_cache():
-    cache = LRU_Cache(5)
+    ((3, [("Jan", 1), ("Feb", 2), ("Mar", 3), ("Apr", 4)], "Mar"),
+     3)
+]
 
-    cache.set(1, 1)
-    cache.set(2, 2)
-    cache.set(3, 3)
-    cache.set(4, 4)
+TEST_ERROR = [
+    (('capacity', [], 1),
+     {'expected_exception': TypeError,
+      'match': "Cache capacity is of type.*. It must be an integer."}),
+
+    ((0, [], 1),
+     {'expected_exception': ValueError,
+      'match': "Cache capacity must be a positive integer."}),
+
+    ((5, [(1, 1), (2, 2), (None, 3)], 1),
+     {'expected_exception': ValueError,
+      'match': "Set key is missing."}),
+
+    ((5, [([1, 2], 1), (2, 2), (3, 3)], 3),
+     {'expected_exception': TypeError,
+      'match': r'Key is of type.*. It is not hashable.'}),
+
+    ((5, [(1, 1), (2, 2), (3, 3)], None),
+     {'expected_exception': KeyError,
+      'match': "Search key is missing."})
+]
+
+
+def create_cache(capacity, lst):
+    cache = LRU_Cache(capacity)
+
+    for key, value in lst:
+        cache.set(key, value)
 
     return cache
 
 
-def test_get():
-    cache = create_cache()
+@pytest.mark.parametrize("input, expected_output", TEST_NORMAL)
+def test_normal(input, expected_output):
+    capacity, lst, key = input
+    cache = create_cache(capacity, lst)
+    assert cache.get(key) == expected_output
 
-    assert cache.get(9) == -1
 
-    assert cache.get(1) == 1
+@pytest.mark.parametrize("input, error", TEST_ERROR)
+def test_error(input, error):
+    capacity, lst, key = input
+    with pytest.raises(**error):
+        cache = create_cache(capacity, lst)
+        cache.get(key)
 
-    with pytest.raises(ValueError, match='Key is missing.'):
-        cache.get(None)
 
-    cache.set(5, 5)
+def test_cache_update():
+    capacity, lst = 5, [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+    cache = create_cache(capacity, lst)
+
+    cache.get(1)
     cache.set(6, 6)
+    assert cache.get(6) == 6
     assert cache.get(1) == 1
     assert cache.get(2) == -1
-
-
-def test_set():
-    cache = create_cache()
-
-    cache.set(5, 5)
-    assert cache.get(5) == 5
-
-    cache.set("June", 6)
-    assert cache.get("June") == 6
-
-    cache.set((1), "January Tuple")
-    assert cache.get((1)) == "January Tuple"
-
-    with pytest.raises(ValueError, match='Key is missing.'):
-        cache.set(None, 1)
-
-    with pytest.raises(TypeError,
-                       match=r'Key is of type.*. It is not hashable.'):
-        cache.set([2], 2)
 
 
 if __name__ == '__main__':
--- Data Structure and Algorithms Nanodegree/Projects/Data Structures/Problem 2 - File Recursion/test.py
+++ Data Structure and Algorithms Nanodegree/Projects/Data Structures/Problem 2 - File Recursion/test.py
@@ -1,47 +1,57 @@
 import pytest
 from main import find_files
 
+TEST_NORMAL = [
+    {
+        'suffix': ".c",
+        'path': "./testdir",
+        'file_paths': (
+            # Windows
+            ['./testdir\\subdir1\\a.c',
+             './testdir\\subdir3\\subsubdir1\\b.c',
+             './testdir\\subdir5\\a.c',
+             './testdir\\t1.c'],
+            # Linux
+            ['./testdir/subdir1/a.c',
+             './testdir/subdir3/subsubdir1/b.c',
+             './testdir/subdir5/a.c',
+             './testdir/t1.c'])
+    },
 
-def test_find_files():
-    suffix = ".c"
-    path = "./testdir"
-    file_paths = (
-        # Windows
-        ['./testdir\\subdir1\\a.c',
-         './testdir\\subdir3\\subsubdir1\\b.c',
-         './testdir\\subdir5\\a.c',
-         './testdir\\t1.c'],
-        # Linux
-        ['./testdir/subdir1/a.c',
-         './testdir/subdir3/subsubdir1/b.c',
-         './testdir/subdir5/a.c',
-         './testdir/t1.c']
-    )
-    assert sorted(find_files(suffix, path)) in file_paths
+    {
+        'suffix': ".gitkeep",
+        'path': "./testdir",
+        'file_paths': (
+            ['./testdir\\subdir2\\.gitkeep', './testdir\\subdir4\\.gitkeep'],
+            ['./testdir/subdir2/.gitkeep', './testdir/subdir4/.gitkeep'])
+    },
 
-    suffix = ".gitkeep"
-    path = "./testdir"
-    file_paths = (
-        ['./testdir\\subdir2\\.gitkeep', './testdir\\subdir4\\.gitkeep'],
-        ['./testdir/subdir2/.gitkeep', './testdir/subdir4/.gitkeep']
-    )
-    assert sorted(find_files(suffix, path)) in file_paths
+    {
+        'suffix': ".c",
+        'path': "./testdir/subdir5",
+        'file_paths': (
+            ['./testdir/subdir5\\a.c'],
+            ['./testdir/subdir5/a.c'])
+    }
+]
 
-    suffix = ".csv"
-    path = "./testdir"
-    assert len(find_files(suffix, path)) == 0
 
-    suffix = ".c"
-    path = "./testdir/subdir5"
-    file_paths = (
-        ['./testdir/subdir5\\a.c'],
-        ['./testdir/subdir5/a.c']
-    )
-    assert find_files(suffix, path) in file_paths
+@pytest.mark.parametrize("dct", TEST_NORMAL)
+def test_normal(dct):
+    assert sorted(find_files(dct['suffix'], dct['path'])) in dct['file_paths']
 
+
+def test_edge():
     suffix = ".c"
     path = "./crazydir"
-    with pytest.raises(IOError, match="No such directory .*"):
+    with pytest.raises(IOError,
+                       match="No such directory .* or it points to a file."):
+        find_files(suffix, path)
+
+    suffix = ".h"
+    path = './testdir/t1.h'
+    with pytest.raises(IOError,
+                       match="No such directory .* or it points to a file."):
         find_files(suffix, path)
 
 
